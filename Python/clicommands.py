444a0a0ab9fa8c3e114bdded0b8c89c0154a4e84
priyanshu24x <prakash24xd@gmail.com>
2026-04-21 18:16:09 +0530

    python basics

---
+++ Python/clicommands.py
@@ -0,0 +1,9 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=4
+>>> b=5
+>>> a==b
+False
+>>> c=a&b
+>>> type(c)
+<class 'int'>
---
+++ Python/firstpgm.py
@@ -0,0 +1,12 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("helo")
+helo
+>>> f="ee"
+>>> printf*5
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    printf*5
+NameError: name 'printf' is not defined. Did you mean: 'print'?
+>>> print(f*5)
+eeeeeeeeee
